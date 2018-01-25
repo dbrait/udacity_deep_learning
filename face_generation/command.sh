00da660f9fd01af8bf35e0c02d941d0ef74c766b
David Brait <david.brait@gmail.com>
2018-01-24 19:48:24 -0500

    Add files via upload

---
+++ face_generation/command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/PhhjvXFfxTbSkoGc9MSvMG' --NotebookApp.token='' --NotebookApp.allow_origin='*'
